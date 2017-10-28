be26717573faf5cedee953742ca85a219263d64b
Timoteo <phillipstr@gmail.com>
2017-10-27 23:11:06 -0400

    Adds cli and core functionality to switch color styles
    User can print a sample JSON output with '-s' option with all
    available preset color styles.  The user can also change the
    default style with the '-d' option.  And, at this point, the
    '-c' options prints out the filepath for the config file should
    a user decide to create their own color style.  Core functionality
    has been updated to accomodate these changes.

--- jsoncolor/cli.py
+++ jsoncolor/cli.py
@@ -8,13 +8,31 @@ from click import argument
 from click import option
 from click import version_option
 
+from jsonconfig.core import Config
 from jsoncut.cli import load_json
 
+from jsoncolor.core import create_style_class
 from jsoncolor.core import format_json
+from jsoncolor.core import get_color_style
 from jsoncolor.core import highlighter
 
 
-def output(data, ctx, indent):
+SAMPLE = {
+    'Style': '',
+    'Number': 1,
+    'Keyword': True,
+    'Array': [
+        {
+            'Number': 2,
+            'Keyword': None,
+            'String': 'This is a string!',
+        },
+    ],
+}
+
+
+def output(data, ctx, indent, style=None):
+    """Output data to stdout."""
     try:
         if data:
             if click._termui_impl.isatty(sys.stdin):
@@ -24,27 +42,74 @@ def output(data, ctx, indent):
                 compact = indent is None
             output = format_json(data, compact, indent)
             if ctx.color and click._termui_impl.isatty(sys.stdout):
-                output = highlighter(output)
+                if style is None:
+                    output = highlighter(output)
+                else:
+                    output = highlighter(output, create_style_class(style))
             click.echo(output)
     except KeyboardInterrupt:
         sys.exit(0)
 
 
+def sample_styles(ctx):
+    """Print SAMPLE in all available preset color styles."""
+    styles = get_color_style(all_colors=True)
+    for style in styles:
+        SAMPLE['Style'] = style
+        output(SAMPLE, ctx, indent=2, style=styles[style])
+
+
+def set_default(style):
+    """Set default color style and save to config file."""
+    styles = get_color_style(all_colors=True)
+    if style not in styles.keys():
+        click.echo('[***] ' + style + ' NOT FOUND!\nAvailable Styles:')
+        for style in styles.keys():
+            click.echo('\t' + style)
+        sys.exit(0)
+
+    with Config('jsoncolor', 'r+') as cfg:
+        cfg.data['default'] = style
+        cfg.kwargs['dump']['indent'] = 4
+    click.echo('Default style set to: ' + style)
+
+
+def create_style():
+    """Print how to create a new color style"""
+    with Config('jsoncolor', 'r') as cfg:
+        path = cfg.filename
+    click.echo('To create a new color style, edit the configuration file')
+    click.echo('in the following path:')
+    click.echo('\n' + path + '\n')
+
+
 @click.command()
 @argument('jsonfile', type=click.Path(readable=True), required=False)
+@option('-c', '--create', is_flag=True, help='Create a new color style')
+@option('-d', '--default', 'default', help='Set default color style')
 @option('-n', '--nocolor', is_flag=True, help='Disable syntax highlighting')
+@option('-s', '--styles', is_flag=True, help='Print all preset styles')
 @version_option(version='0.0', prog_name='JSON Color')
 @click.pass_context
 def main(ctx, **kwds):
     """JSON text coloring."""
     ctx.color = False if kwds['nocolor'] else True
 
-    if not kwds['jsonfile']:
+    if not kwds['jsonfile'] and not (kwds['styles'] or kwds['default']):
         if click._termui_impl.isatty(sys.stdin):
             click.echo(ctx.get_usage())
             click.echo('Try `jsoncolor --help` for more information.')
             sys.exit(0)
 
-    data = load_json(ctx, kwds['jsonfile'])
+    if kwds['styles']:
+        sample_styles(ctx)
+        sys.exit(0)
 
+    if kwds['default']:
+        set_default(kwds['default'])
+
+    if kwds['create']:
+        create_style()
+
+    data = load_json(ctx, kwds['jsonfile'])
     output(data, ctx, indent=2)
--- jsoncolor/core.py
+++ jsoncolor/core.py
@@ -14,40 +14,85 @@ from pygments.lexers import JsonLexer
 from jsonconfig import Config
 
 
-def get_config_color():
-    """Gets color scheme from jsoncolor config file."""
+def get_color_style(all_colors=False):
+    """
+    Gets color style from jsoncolor config file.
+
+    Args:
+        * all_colors (boolean): return singleton value of all values
+
+    Returns:
+        * default color scheme if all_colors=False
+        * dict of all preset colors if all_colors=True
+    """
     with Config('jsoncolor', 'r') as cfg:
-        data = cfg.data
-    return data['styles'][data['default']]
+        colors = cfg.data
+    if not all_colors:
+        return colors['styles'][colors['default']]
+    else:
+        return colors['styles']
 
 
-STYLE = get_config_color()
+STYLE = get_color_style()
 
 
-def format_json(d, compact=False, indent=2):
-    """Format JSON; compact or indented."""
+def create_style_class(style):
+    """
+    Create a Style Class.
+
+    Args:
+        * style (dict): color style dict with keywords:
+            Token, Keyword, Name_Tag, String, Number
+
+    Returns:
+        * StyleClass with appropriate color set for pygments
+            Terminal256Formatter
+    """
+
+    class StyleClass(pygments.style.Style):
+        styles = {
+            pygments.token.Token: style['Token'],
+            pygments.token.Keyword: style['Keyword'],
+            pygments.token.Name.Tag: style['Name_Tag'],
+            pygments.token.String: style['String'],
+            pygments.token.Number: style['Number'],
+        }
+
+    return StyleClass
+
+
+def format_json(data, compact=False, indent=2):
+    """
+    Format Dict to JSON.
+
+    Args:
+        * data (dict): python dict
+        * compact (boolean): non-indended output
+        * indent (int): set indenting for compact=False mode
+
+    Returns:
+        * data formatted to json-serialized content
+    """
     separators = (',', ':') if compact else None
-    return json.dumps(d, indent=indent, separators=separators)
+    return json.dumps(data, indent=indent, separators=separators)
 
 
-def highlighter(d, style=STYLE):
-    """JSON Syntax highlighter."""
+def highlighter(data, style=create_style_class(STYLE)):
+    """
+    JSON Syntax Highlighter.
+
+    Args:
+        * data (json): json-serialized content
+        * style (StyleClass): Style to be used by Terminal256Formatter,
+            return value from create_style_class()
+
+    Returns:
+        * Json-serialized content with proper color coding
+        * Json-serialized content without color coding if a NameError or
+            AttributeError from Terminal256Formatter is thrown.
+    """
     try:
-        formatter = Terminal256Formatter(style=StyleClass)
+        formatter = Terminal256Formatter(style=style)
     except (NameError, AttributeError):
-        return d
-    return pygments.highlight(d, JsonLexer(), formatter)
-
-
-class StyleClass(pygments.style.Style):
-    """
-    Create a style to be used by pygments by reading in default color
-    style from jsoncolor config file.
-    """
-    styles = {
-        pygments.token.Token: STYLE['Token'],
-        pygments.token.Keyword: STYLE['Keyword'],
-        pygments.token.Name.Tag: STYLE['Name_Tag'],
-        pygments.token.String: STYLE['String'],
-        pygments.token.Number: STYLE['Number'],
-    }
+        return data
+    return pygments.highlight(data, JsonLexer(), formatter)
