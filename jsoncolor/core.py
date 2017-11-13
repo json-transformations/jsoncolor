02e477ccb8f53569d35d6f2f3091cecf36f167c5
Timoteo <phillipstr@gmail.com>
2017-11-12 22:45:31 -0500

    modifies docstrings to remove astrisks for args

--- jsoncolor/core.py
+++ jsoncolor/core.py
@@ -20,11 +20,11 @@ def get_color_style(all_colors=False):
     Gets color style from jsoncolor config file.
 
     Args:
-        * all_colors (boolean): return singleton value of all values
+        all_colors (boolean): return singleton value of all values
 
     Returns:
-        * default color scheme if all_colors=False
-        * dict of all preset colors if all_colors=True
+        default color scheme if all_colors=False
+        dict of all preset colors if all_colors=True
     """
     with Config('jsoncolor', 'r') as cfg:
         colors = cfg.data
@@ -42,11 +42,11 @@ def validate_style(style):
     Validates to see if a given style has valid values.
 
     Args:
-        * style (dict): color style dict with values as hexadecimal color
+        style (dict): color style dict with values as hexadecimal color
             codes, as found at: http://www.colorhexa.com/
 
     Returns:
-        * style (dict): values that are not valid will be deleted and a
+        style (dict): values that are not valid will be deleted and a
             truncated dict will be returned.  All valid values will remain.
     """
     valid = {}
@@ -61,11 +61,11 @@ def create_style_class(style):
     Create a Style Class.
 
     Args:
-        * style (dict): color style dict with keywords:
+        style (dict): color style dict with keywords:
             Token, Keyword, Name_Tag, String, Number
 
     Returns:
-        * StyleClass with appropriate color set for pygments
+        StyleClass with appropriate color set for pygments
             Terminal256Formatter
     """
     style = validate_style(style)
@@ -87,12 +87,12 @@ def format_json(data, compact=False, indent=2):
     Format Dict to JSON.
 
     Args:
-        * data (dict): python dict
-        * compact (boolean): non-indended output
-        * indent (int): set indenting for compact=False mode
+        data (dict): python dict
+        compact (boolean): non-indended output
+        indent (int): set indenting for compact=False mode
 
     Returns:
-        * data formatted to json-serialized content
+        data formatted to json-serialized content
     """
     separators = (',', ':') if compact else None
     return json.dumps(data, indent=indent, separators=separators)
@@ -103,13 +103,13 @@ def highlighter(data, style=create_style_class(STYLE)):
     JSON Syntax Highlighter.
 
     Args:
-        * data (json): json-serialized content
-        * style (StyleClass): Style to be used by Terminal256Formatter,
+        data (json): json-serialized content
+        style (StyleClass): Style to be used by Terminal256Formatter,
             return value from create_style_class()
 
     Returns:
-        * Json-serialized content with proper color coding
-        * Json-serialized content without color coding if a ValueError
+        Json-serialized content with proper color coding
+        Json-serialized content without color coding if a ValueError
             from Terminal256Formatter is thrown.
     """
     try:
